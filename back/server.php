0994617d11b8bc06ad66242b15e63fdc432baff7
Nislupko <nislupko@gmail.com>
2018-11-06 18:48:27 +0300

    Back finished. Front has cabinet. Functions for cabinet added

--- back/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*require "db.php";
-db::connect();
-$arr = $_POST;
-echo "INSERT INTO user('name', 'email', 'password') VALUES ('"
-    .$arr['name']."','"
-    .$arr['email']."','"
-    .hash('md5',$arr['password'])."')";
-echo db::foo(array('email' => $_POST['email'], 'password'=>$_POST['password'], 'name'=>$_POST['name']));*/
